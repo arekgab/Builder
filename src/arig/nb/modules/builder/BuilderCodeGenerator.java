e75e958d4e56da4c9fa72ce784ae6f9fe01ef628
Arek <arkadiusz.gabiga@gmail.com>
2013-02-04 18:48:12 +0100

    BSD-3 license

--- src/arig/nb/modules/builder/BuilderCodeGenerator.java
+++ src/arig/nb/modules/builder/BuilderCodeGenerator.java
@@ -1,10 +1,28 @@
 /* 
- * This software is released as-is for the general good. It may be used without 
- * permission for any reason as long as my name is contained in the documentation 
- * somewhere. I don't care if it's highly visible or not. 
- * I claim no responsibility for damages that may occur to any person, place, 
- * thing, idea or noun through the direct or indirect use, misuse or 
- * disuse of this product
+ * Copyright (c) 2013, Arkadiusz Gabiga
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Arkadiusz Gabiga nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ARKADIUSZ GABIGA BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package arig.nb.modules.builder;
 
